e9f19e974cc4d3950f0b3677625162ebdecc207e
artizon-io <mingsum.sam@gmail.com>
2022-10-23 16:11:12 +0800

    fix(electron): load esm from cjs

--- electron-ts/index.cjs
+++ electron-ts/index.cjs
@@ -1,3 +1,5 @@
+require("fix-esm").register();
+
 // Set options as a parameter, environment variable, or rc file.
 require = require("esm")(module/*, options*/)
 module.exports = require("./after-ts/electron-ts/src/render/index.js")
